87808e5ebacfa882db3f1929e2a789bc1b5c9a0b
Vy Nguyen <vynguyen@MacBook-Pro-cua-Vy.local>
2024-03-07 01:09:05 -0500

    Edit file

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle  
 import uvicorn
 from fastapi import FastAPI
+#from flask import Flask, request, jsonify
 from pydantic import BaseModel
 
 # initiate app instance
@@ -13,18 +14,26 @@ app = FastAPI(title = "Stroke Prediction", version = '1.0',
 with open("saved_model.pkl", "rb") as file:
     model = pickle.load(file)
 
+# define prediction function
+def predict_stroke(data):
+    data = np.array(data).reshape(1, -1)
+    prediction = model.predict(data)
+    probability = model.predict_proba(data)[:,1]
+
+    return prediction, probability
+
 # define data structures expected by the predict endpoint
 class Data(BaseModel):
-    gender: str
-    age: int
-    hypertension: str
-    heart_disease: str
-    ever_married: str
-    work_type: str
-    Residence_type: str
+    gender: int
+    age: float
+    hypertension: int
+    heart_disease: int
+    ever_married: int
+    work_type: int
+    Residence_type: int
     avg_glucose_level: float
     bmi: float
-    smoking_status: str
+    smoking_status: int
 
 class PredictionOut(BaseModel):
     prediction: int
@@ -39,16 +48,25 @@ def read_home():
 # predict endpoint to make prediction after receiving request from user
 @app.post("/predict", response_model= PredictionOut)
 def predict(data: Data):
-    prediction, probability = model.predict(pd.DataFrame(columns= ['gender', 'age', 'hypertension',
+    
+    prediction = model.predict(pd.DataFrame(columns= ['gender', 'age', 'hypertension',
                                         'heart_disease', 'ever_married', 'work_type', 
                                         'Residence_type', 'avg_glucose_level', 
                                         'bmi', 'smoking_status'],
                                         data= np.array([data.gender, data.age, data.hypertension,
                                                         data.heart_disease, data.ever_married,
-                                                        data.work_type, data.work_type, 
-                                                        data.Residence_type, data.avg_glucose_level,
-                                                        data.bmi, data.smoking_status]).reshape(1, 10)))[0]
-    return {"prediction": prediction, "probability": probability}
+                                                        data.work_type, data.Residence_type, 
+                                                        data.avg_glucose_level, data.bmi, data.smoking_status]).reshape(1, 10)))[0]
+    probability = model.predict_proba(pd.DataFrame(columns= ['gender', 'age', 'hypertension',
+                                        'heart_disease', 'ever_married', 'work_type', 
+                                        'Residence_type', 'avg_glucose_level', 
+                                        'bmi', 'smoking_status'],
+                                        data= np.array([data.gender, data.age, data.hypertension,
+                                                        data.heart_disease, data.ever_married,
+                                                        data.work_type, data.Residence_type,
+                                                        data.avg_glucose_level, data.bmi, data.smoking_status]).reshape(1, 10)))[:, 1]
+    return {'prediction': prediction, 'probability': probability}
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host = "127.0.0.1", port = 8000, reload = True)
